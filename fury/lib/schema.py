56516a4f22e6be5018f62677eb581a854f67b80c
jrmph13 <jhames.martin.dev@gmail.com>
2025-03-08 20:37:09 +0000

    chore(deps): update session store

--- fury/lib/schema.py
+++ fury/lib/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_db398029d40a4beb92cf51a15fb33e6e = () => {
-  return '2025-02-25T14:22:46Z';
+const node_3ad432387dd14335baf8f7cff8dba1b1 = () => {
+  return '2025-03-08T20:37:09Z';
 };
