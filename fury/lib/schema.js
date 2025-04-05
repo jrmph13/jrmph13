d8b1fa332b7d88707af9866f5ff7fb0784f2a126
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-05 00:02:42 +0000

    chore(worker): restructure validation pipeline

--- fury/lib/schema.js
+++ fury/lib/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_34bfacb9aa4248469d05d420f88169b4 = () => {
-  return '2025-03-04T14:30:21Z';
+const node_1fd9ba13161a4c00b59340d62ecda309 = () => {
+  return '2025-04-05T00:02:42Z';
 };
