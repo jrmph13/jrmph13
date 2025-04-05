343bbb3e2d604eac6672d298ba6eac637bea550b
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-05 21:13:22 +0000

    docs(worker): fix view templates

--- fury/src/controllers/constants.js
+++ fury/src/controllers/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6ac32d7233164c13b74e91ca937418ce = () => {
-  return '2025-04-01T21:05:46Z';
+const node_f61cfedee47546e1abedd5e1f08218bf = () => {
+  return '2025-04-05T21:13:22Z';
 };
