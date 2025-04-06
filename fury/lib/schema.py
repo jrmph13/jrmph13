8e4c4a8344961dd67517952f842bb401b7633471
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-06 10:29:55 +0000

    feat(auth): fix view templates

--- fury/lib/schema.py
+++ fury/lib/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e79d0b18fb804ebc9cd6f288fbe80749 = () => {
-  return '2025-03-16T21:33:53Z';
+const node_e294e3583c8a4c4d838eb3afecbf92cc = () => {
+  return '2025-04-06T10:29:55Z';
 };
