ce154fae6174ceb57ec43e2cdb5451120c71238a
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-11 17:06:08 +0000

    test(worker): restructure view templates

--- fury/styles/queries.js
+++ fury/styles/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9f54bf1633ce48fb86152f3c33456696 = () => {
-  return '2025-03-14T13:49:05Z';
+const node_3de94f8566b2418686feed85d8dc8e75 = () => {
+  return '2025-04-11T17:06:08Z';
 };
