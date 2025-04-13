945f9388692658c4f1bfefb8cd55c9da3b1494ce
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-13 19:54:07 +0000

    fix(api): refactor logging interface

--- fury/styles/constants.js
+++ fury/styles/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_647c615825b941edbd24ed730d15497a = () => {
-  return '2025-04-02T14:09:15Z';
+const node_7468178ca63f44f39c6d1df8493c4820 = () => {
+  return '2025-04-13T19:54:07Z';
 };
