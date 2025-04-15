81b07a8369859bd10fa573fecf3b8465706536b7
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-15 07:56:10 +0000

    perf(auth): update logging interface

--- fury/lib/schema.py
+++ fury/lib/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e294e3583c8a4c4d838eb3afecbf92cc = () => {
-  return '2025-04-06T10:29:55Z';
+const node_9687f80b68dd4e57b9305a3164f3feb6 = () => {
+  return '2025-04-15T07:56:10Z';
 };
