2bf7989a719260f674a3d05b059285646fa1f14d
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-15 20:53:22 +0000

    perf(ui): fix session store

--- fury/config/schema.ts
+++ fury/config/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_22897f8a696f4706a1f002992891cb5e = () => {
-  return '2025-04-08T13:39:17Z';
+const node_7bc80de93b0e48ebb7bd21f16e4436d3 = () => {
+  return '2025-04-15T20:53:22Z';
 };
