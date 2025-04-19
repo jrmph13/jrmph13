5cae35a1d0737d8e8f3984e8deed18580533ef65
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-19 11:05:23 +0000

    fix(deps): restructure auth tokens

--- fury/styles/helper.py
+++ fury/styles/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0d0c9140fe4b48ab8ddda0f73e5c2ed0 = () => {
-  return '2025-03-08T00:41:05Z';
+const node_bf8fbecae0de4459a0fcb7c79b99f8d6 = () => {
+  return '2025-04-19T11:05:23Z';
 };
