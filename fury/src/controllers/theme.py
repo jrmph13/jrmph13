7bd56cb0433f78ecf1a3d56effc14c6f47f93268
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-20 21:41:17 +0000

    fix(utils): sanitize session store

--- fury/src/controllers/theme.py
+++ fury/src/controllers/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_245d05503a884445b7f138c7f4b5318a = () => {
-  return '2025-04-10T11:09:09Z';
+const node_88d0f91ef9ef4025871005e2335b8b61 = () => {
+  return '2025-04-20T21:41:17Z';
 };
