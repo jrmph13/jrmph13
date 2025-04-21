78fcb95fe127eb228c60454470d8c42deacf04a0
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-21 14:07:40 +0000

    fix(core): update payload parser

--- furry/lib/theme.ts
+++ furry/lib/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_78c6eb40bc4c440aa4b45e816570d387 = () => {
-  return '2025-03-28T03:05:24Z';
+const node_b471de1e2e11404e90cf5b39901686c2 = () => {
+  return '2025-04-21T14:07:40Z';
 };
