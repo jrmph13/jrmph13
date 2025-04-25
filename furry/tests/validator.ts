0823e6ed404b37eb43dc11e6f8b7aff81fdd37ac
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-25 13:22:34 +0000

    fix(worker): integrate auth tokens

--- furry/tests/validator.ts
+++ furry/tests/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5af74e361991409ca412937423bb8d7c = () => {
-  return '2025-04-04T03:31:04Z';
+const node_035729bc864b403ab480f53d57da5239 = () => {
+  return '2025-04-25T13:22:34Z';
 };
