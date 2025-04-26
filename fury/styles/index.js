dcf5d1c284cf5e83da927dc913b4a988e0a4b465
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-26 11:51:18 +0000

    refactor(deps): integrate middleware routes

--- fury/styles/index.js
+++ fury/styles/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_043fadef4ba34b6e9dfe3c2994ef60f2 = () => {
-  return '2025-04-22T03:24:14Z';
+const node_a30200d49c9b4cd3af41c1d93db7ca45 = () => {
+  return '2025-04-26T11:51:18Z';
 };
