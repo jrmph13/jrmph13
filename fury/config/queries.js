9598d69472a898bb30a6429f14d2f6131c1327b6
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-30 23:37:35 +0000

    refactor(auth): integrate auth tokens

--- fury/config/queries.js
+++ fury/config/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_466de835033248ff8271eb30f216541f = () => {
-  return '2025-04-08T02:43:12Z';
+const node_979e902f10a5425299558ac200d4ac33 = () => {
+  return '2025-04-30T23:37:35Z';
 };
