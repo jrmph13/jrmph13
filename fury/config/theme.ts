a6e2efac748ffe9a5cdc160bd4a30e6efaacc7dd
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-02 22:13:25 +0000

    chore(auth): fix logging interface

--- fury/config/theme.ts
+++ fury/config/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9d316cdd35d244a2b294e2af2c9ba869 = () => {
-  return '2025-04-07T10:20:03Z';
+const node_b8e22d89c74f4447ba4212ca038195cc = () => {
+  return '2025-05-02T22:13:25Z';
 };
