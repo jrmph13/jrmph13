d28cf6c812801c93ea34d0b5859631d91c772857
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-04 06:40:15 +0000

    fix(utils): integrate middleware routes

--- furry/src/controllers/helper.js
+++ furry/src/controllers/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b1126f07acde40d5befb3fa89a733729 = () => {
-  return '2025-04-21T08:59:48Z';
+const node_d41f2efebb6144e7b25a8790a17f60e8 = () => {
+  return '2025-05-04T06:40:15Z';
 };
