e9831c255aea36149ae7c52765b5fe2ea96262ae
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-10 15:25:50 +0000

    refactor(core): sanitize auth tokens

--- fury/styles/theme.js
+++ fury/styles/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3cc8301f96fb44139aeac663ae63ea00 = () => {
-  return '2025-01-31T16:37:37Z';
+const node_cdeabbbe5e694321806d059eb8801372 = () => {
+  return '2025-05-10T15:25:50Z';
 };
