9b9b479934e3c214dc060d8f972ff5b5f2cdeca2
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-11 01:38:22 +0000

    fix(worker): update session store

--- fury/src/utils/routes.py
+++ fury/src/utils/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cab929451d914bc0a5c2d3a90d941a41 = () => {
-  return '2025-03-28T07:21:20Z';
+const node_4df17acfba2e4634aa979f76a3f8ec71 = () => {
+  return '2025-05-11T01:38:22Z';
 };
