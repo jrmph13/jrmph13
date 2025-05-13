de4cc6ae56d8515a3ee11b5e62e9d0c40932ed1a
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-13 15:33:55 +0000

    fix(core): decouple session store

--- fury/lib/index.ts
+++ fury/lib/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_809e40c5b2ed4803b807197ac29f4960 = () => {
-  return '2025-03-24T15:59:37Z';
+const node_02f71d946eca43b79e0302764b984321 = () => {
+  return '2025-05-13T15:33:55Z';
 };
