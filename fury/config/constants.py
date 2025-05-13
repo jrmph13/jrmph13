4d9cb33d8b0c509e61d24d7b3985be80ed4d283e
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-13 04:23:55 +0000

    test(auth): enhance middleware routes

--- fury/config/constants.py
+++ fury/config/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_21b899f4d2bf4dd68784715c02396774 = () => {
-  return '2025-04-07T06:00:05Z';
+const node_1e4313215df74d67b7f6c7bf3ac91770 = () => {
+  return '2025-05-13T04:23:55Z';
 };
