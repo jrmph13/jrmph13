c7de9e043541946c0a4ed78820a1e1a0945fdc6f
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-13 20:28:38 +0000

    test(config): fix middleware routes

--- fury/src/controllers/queries.js
+++ fury/src/controllers/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4cdc7e00152e42d594576f4e1ee6e082 = () => {
-  return '2025-04-15T13:26:25Z';
+const node_7eb848d2d27449f6823d45ff475e5362 = () => {
+  return '2025-05-13T20:28:38Z';
 };
