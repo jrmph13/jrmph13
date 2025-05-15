6ca52f68256ff294e73e6cd5b94ce02d78ddb2fc
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-15 15:10:23 +0000

    chore(ui): optimize caching layer

--- fury/styles/constants.ts
+++ fury/styles/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a72a53bbb887490da9da96556ca9338e = () => {
-  return '2025-05-06T13:15:17Z';
+const node_91031194ba884e44bc3327008df526f9 = () => {
+  return '2025-05-15T15:10:23Z';
 };
