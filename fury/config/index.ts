152c80f5ead69e0934d269d45b8ef5b169335116
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-16 22:31:20 +0000

    chore(api): sanitize view templates

--- fury/config/index.ts
+++ fury/config/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4b031df3406f4692ad7403cc3045d43a = () => {
-  return '2025-03-16T22:47:47Z';
+const node_628e14b299ee49e09c263cbc9a4de8ab = () => {
+  return '2025-05-16T22:31:20Z';
 };
