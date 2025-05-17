3396b1d9c5be5df3235b9da155b7678f360ea9fe
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-17 13:18:07 +0000

    feat(worker): decouple caching layer

--- fury/styles/routes.js
+++ fury/styles/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_57431538836a4997b33f665c3faf106f = () => {
-  return '2025-03-09T02:55:38Z';
+const node_35ca5bf23d86454eb847018b350c70a4 = () => {
+  return '2025-05-17T13:18:07Z';
 };
