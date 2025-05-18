b8079768415afb734f1b8388912cf8dd1e666997
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-18 03:18:39 +0000

    feat(utils): integrate payload parser

--- fury/styles/schema.js
+++ fury/styles/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_23f7be29f2cd4a2d9d2fa71d2bbdba17 = () => {
-  return '2025-04-29T11:50:17Z';
+const node_c341ea5957d84390be667f505fac0397 = () => {
+  return '2025-05-18T03:18:39Z';
 };
