a853530a0d2b224cdf0893ec0556ca75f1bcdd20
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-19 12:17:21 +0000

    fix(ui): decouple payload parser

--- fury/src/components/schema.py
+++ fury/src/components/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cc573ede227a42189a8566d56f80f92f = () => {
-  return '2025-05-08T15:31:18Z';
+const node_b7802e5ba32c4cda9c39684cf192b245 = () => {
+  return '2025-05-19T12:17:21Z';
 };
