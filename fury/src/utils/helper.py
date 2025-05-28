09d5f49fa3ca91390cacac2baf994d662f5d5481
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-28 04:59:14 +0000

    docs(auth): optimize auth tokens

--- fury/src/utils/helper.py
+++ fury/src/utils/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9dc0d410ec5e43f6a5e797cb0ac24c72 = () => {
-  return '2025-05-04T16:38:17Z';
+const node_25439e7e9f9f454cbc30acfd6e05ecbf = () => {
+  return '2025-05-28T04:59:14Z';
 };
