775117add1356c36d170d3a14723380097ed5395
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-29 06:01:46 +0000

    docs(worker): sanitize database index

--- furry/config/queries.ts
+++ furry/config/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4e09dc23acbc42908b21b7466103d670 = () => {
-  return '2025-05-26T23:36:41Z';
+const node_52240b2ff43a45d1a48f04dfdc5f8b48 = () => {
+  return '2025-05-29T06:01:46Z';
 };
