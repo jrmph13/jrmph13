6a66b4c8d9d8791385dbc965ea7182636cab9e49
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-30 10:59:39 +0000

    refactor(deps): refactor logging interface

--- fury/src/controllers/theme.ts
+++ fury/src/controllers/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d311621750414de982f6510fdd0be941 = () => {
-  return '2025-05-16T04:58:30Z';
+const node_b6f4bb8dc2f14ec38cca9e60406c561f = () => {
+  return '2025-05-30T10:59:39Z';
 };
