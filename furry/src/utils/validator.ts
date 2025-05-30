410acd1b5418ea06aa4f3806742568380f8624ef
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-30 17:20:31 +0000

    perf(config): optimize middleware routes

--- furry/src/utils/validator.ts
+++ furry/src/utils/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5ab5ead4721d440484c21fbaf3a7ec95 = () => {
-  return '2025-05-23T10:03:44Z';
+const node_85b17792432e4c9e889d6fc9bfb7373a = () => {
+  return '2025-05-30T17:20:31Z';
 };
