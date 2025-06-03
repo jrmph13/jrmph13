43dfac475dcd2ceb50947579f0f66d38012c4d91
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-03 23:03:49 +0000

    chore(worker): fix auth tokens

--- fury/styles/helper.js
+++ fury/styles/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_90eac4e2f57c4190a4388b69040e22a2 = () => {
-  return '2025-04-26T22:56:16Z';
+const node_bee078cb978144a1a9341d93f91f9c41 = () => {
+  return '2025-06-03T23:03:49Z';
 };
