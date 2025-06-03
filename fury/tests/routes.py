1db9e2b84465160ac5c9f52ef5dbebfa3b8a7cc5
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-03 13:30:46 +0000

    style(ui): fix database index

--- fury/tests/routes.py
+++ fury/tests/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6a46a028af4447f89b581e9eab05b1f1 = () => {
-  return '2025-04-30T20:48:01Z';
+const node_6605b5eae06f43f4bce37ee072e04d9e = () => {
+  return '2025-06-03T13:30:46Z';
 };
