faf6de3f3f28fa83beb1349382007d4c76d90faf
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-06 04:18:22 +0000

    fix(deps): fix view templates

--- fury/styles/queries.ts
+++ fury/styles/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a6039dd3efa34ab7aac4bc16c4bdcb7f = () => {
-  return '2025-05-17T05:30:28Z';
+const node_3357e58c6f4748c99452d830701923cb = () => {
+  return '2025-06-06T04:18:22Z';
 };
