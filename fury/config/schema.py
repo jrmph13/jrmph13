7f0b91bede059741e488f810cf916e79a2520271
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-09 23:18:33 +0000

    refactor(worker): integrate view templates

--- fury/config/schema.py
+++ fury/config/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_129ccf1631e344ffa40b8c8675f48ead = () => {
-  return '2025-05-07T15:13:05Z';
+const node_18249dad147e442abe20033ccb980ea5 = () => {
+  return '2025-06-09T23:18:33Z';
 };
