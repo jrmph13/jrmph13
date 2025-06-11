b43b1db56ac4c59620f0b64c68ea783a19902094
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-11 04:11:07 +0000

    fix(utils): update auth tokens

--- furry/src/controllers/helper.ts
+++ furry/src/controllers/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2d3c98a890bb4c84b2453a748f11cc40 = () => {
-  return '2025-05-13T09:40:25Z';
+const node_52c86cd68d074533983fe0b688fa58a7 = () => {
+  return '2025-06-11T04:11:07Z';
 };
