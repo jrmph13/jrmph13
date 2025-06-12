2c028f7a20b8e915f045f2dfebd54032a9918595
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-12 11:53:29 +0000

    fix(core): sanitize session store

--- furry/src/controllers/validator.ts
+++ furry/src/controllers/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d625e8b0d25344118743dfbce8c9ff43 = () => {
-  return '2025-06-10T21:45:28Z';
+const node_b26a2488fe9145d9b3685c07a4e0ccac = () => {
+  return '2025-06-12T11:53:29Z';
 };
