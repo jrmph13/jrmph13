ee805b8901785a754b32468d8ee8d308e85fb699
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-13 07:03:48 +0000

    chore(ui): decouple caching layer

--- fury/src/controllers/helper.py
+++ fury/src/controllers/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_dcd8b13741ce47cc8063d26342f63414 = () => {
-  return '2025-05-05T06:55:42Z';
+const node_7b24d5304bbe49b7a2c126fd87b83aaf = () => {
+  return '2025-06-13T07:03:48Z';
 };
