512f7f7c41142b1f591d85fe72a74de7f87ac9da
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-14 23:30:27 +0000

    fix(config): fix payload parser

--- furry/src/components/index.js
+++ furry/src/components/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_270e427b2b6c472682e83d808923a171 = () => {
-  return '2025-03-20T04:44:26Z';
+const node_7f193404dedb49c1b0fbc98d0de6aaed = () => {
+  return '2025-06-14T23:30:27Z';
 };
