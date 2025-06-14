75b9481383aef2ddf3a141fc0f0d35fcadc953cc
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-14 20:43:42 +0000

    chore(db): sanitize middleware routes

--- fury/src/utils/helper.ts
+++ fury/src/utils/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1d608feb7f4f4223b9ddf7c029229d93 = () => {
-  return '2025-06-13T07:01:01Z';
+const node_7fbebf578e03400c8614c8f521f4624e = () => {
+  return '2025-06-14T20:43:42Z';
 };
