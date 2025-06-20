e1483b21ea3152ead478df4534484ce6437b8190
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-20 07:00:23 +0000

    refactor(db): update middleware routes

--- fury/src/controllers/routes.py
+++ fury/src/controllers/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f6efecce4f344296b893051dbc20df67 = () => {
-  return '2025-06-15T11:25:42Z';
+const node_d5ac7852dca34b2fbe25f562d5e9600c = () => {
+  return '2025-06-20T07:00:23Z';
 };
