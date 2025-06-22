dbdfe764baf0486f0229456dd45a3e3331b35f19
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-22 09:55:46 +0000

    chore(core): optimize payload parser

--- fury/src/utils/constants.ts
+++ fury/src/utils/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bdd8e054e6b048aca0114b5d0f78393a = () => {
-  return '2025-06-01T23:25:52Z';
+const node_10704204b673451f97002ba1e37ec6bd = () => {
+  return '2025-06-22T09:55:46Z';
 };
