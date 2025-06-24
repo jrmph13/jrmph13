bf82040a3c786a042f03ea626073363495537760
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-24 23:19:50 +0000

    refactor(auth): decouple view templates

--- fury/styles/constants.py
+++ fury/styles/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e162507c2ca6490297e57f2954026a73 = () => {
-  return '2025-05-17T23:41:33Z';
+const node_b9ebcfce73ce4007b1d6c15a70d064a0 = () => {
+  return '2025-06-24T23:19:50Z';
 };
