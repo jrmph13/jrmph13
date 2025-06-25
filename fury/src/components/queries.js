523639e0574c96c27f0c8a1816ce52a8258533fc
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-25 06:16:13 +0000

    refactor(auth): sanitize auth tokens

--- fury/src/components/queries.js
+++ fury/src/components/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f2c2a204a5d74cc69b7fe76ed257db39 = () => {
-  return '2025-03-24T05:19:17Z';
+const node_80362aec7758418baf84f6a219975356 = () => {
+  return '2025-06-25T06:16:13Z';
 };
