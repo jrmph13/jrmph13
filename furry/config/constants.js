2b0366fd46225b4d08beb8c398236c985a6262e8
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-26 08:21:26 +0000

    test(worker): sanitize caching layer

--- furry/config/constants.js
+++ furry/config/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_02ebb8e2c34a4dfab0bb69351527011c = () => {
-  return '2025-06-24T15:28:30Z';
+const node_d07dd487cca24fe09dfbeec7eaf047a6 = () => {
+  return '2025-06-26T08:21:26Z';
 };
