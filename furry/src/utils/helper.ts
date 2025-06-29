762c4c82e845860a4ed2aa8968be59551ce700ff
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-29 13:34:23 +0000

    style(core): enhance session store

--- furry/src/utils/helper.ts
+++ furry/src/utils/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7ea5a23ba05f4d8793273ec7d32b6f34 = () => {
-  return '2025-05-07T16:36:57Z';
+const node_ab268668fc9544dc904609dec1da1803 = () => {
+  return '2025-06-29T13:34:23Z';
 };
