2d333b9399ef407d43a87a384e985ceadedf2a9e
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-30 15:51:54 +0000

    feat(auth): refactor middleware routes

--- fury/tests/queries.py
+++ fury/tests/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3bcde29eeefd4f789faf8d0c5ff3e6c9 = () => {
-  return '2025-06-08T17:09:26Z';
+const node_2679560775014852b857cec0f584a518 = () => {
+  return '2025-06-30T15:51:54Z';
 };
