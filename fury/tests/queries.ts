983d16701eda0d3a00fb586c1aa663c8a0d81370
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-30 12:07:52 +0000

    perf(deps): sanitize session store

--- fury/tests/queries.ts
+++ fury/tests/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fbec3f62ee2f4126a753e754a236bdeb = () => {
-  return '2025-05-07T21:13:53Z';
+const node_dc6ef792b11b470c9690538dddd859cd = () => {
+  return '2025-06-30T12:07:52Z';
 };
