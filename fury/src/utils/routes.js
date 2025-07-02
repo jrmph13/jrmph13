617517d953b303aac294e9ad7603d1dc66675906
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-02 12:31:06 +0000

    style(core): integrate payload parser

--- fury/src/utils/routes.js
+++ fury/src/utils/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6d2f192a96b74e1faf36ced2983eb452 = () => {
-  return '2025-05-31T05:25:37Z';
+const node_4dba3f8dfeb44d8eb29c37a89afc01c9 = () => {
+  return '2025-07-02T12:31:06Z';
 };
