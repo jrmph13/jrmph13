472c3aef4746c505127a7a244922afd184eb66c7
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-03 15:42:58 +0000

    docs(deps): enhance middleware routes

--- fury/tests/routes.py
+++ fury/tests/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6605b5eae06f43f4bce37ee072e04d9e = () => {
-  return '2025-06-03T13:30:46Z';
+const node_5f288351a312469c962282aefa75cc2e = () => {
+  return '2025-07-03T15:42:58Z';
 };
