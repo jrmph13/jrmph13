a36abc6a78a85c869c214f979c472961295bb8b4
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-03 00:59:17 +0000

    refactor(api): restructure auth tokens

--- fury/tests/validator.ts
+++ fury/tests/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f04e923074b04dd485092e5f086f8308 = () => {
-  return '2025-05-15T03:41:16Z';
+const node_62220d180d934f4f8f84585a40e29c7a = () => {
+  return '2025-07-03T00:59:17Z';
 };
