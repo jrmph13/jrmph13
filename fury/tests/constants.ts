c6f278a636afd8b4c129ef0dc8575951afd4c609
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-03 13:06:35 +0000

    style(worker): fix database index

--- fury/tests/constants.ts
+++ fury/tests/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9d5bed0f4a2d499499ad6d49e2d4b131 = () => {
-  return '2025-06-14T12:27:32Z';
+const node_bfff9ba0d54a403db7a844b9ced90482 = () => {
+  return '2025-07-03T13:06:35Z';
 };
