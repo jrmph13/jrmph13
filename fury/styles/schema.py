86f8dda254739ac42195b73ab194e43345ac187f
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-03 07:01:34 +0000

    docs(utils): restructure auth tokens

--- fury/styles/schema.py
+++ fury/styles/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2491562133e547ddb5e541e4f8ac994e = () => {
-  return '2025-06-30T11:29:46Z';
+const node_2ba9da88807b468988a1c8c0a96ef27e = () => {
+  return '2025-07-03T07:01:34Z';
 };
