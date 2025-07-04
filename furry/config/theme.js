7c6a370d6f58c3001d936799dde5cd9b16b7d7a7
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-04 14:08:42 +0000

    docs(deps): update payload parser

--- furry/config/theme.js
+++ furry/config/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7e77af8d47384670864bc40685d62ab3 = () => {
-  return '2025-06-19T09:45:14Z';
+const node_71ddd46532cc423b8db0dc5ad3095f57 = () => {
+  return '2025-07-04T14:08:42Z';
 };
