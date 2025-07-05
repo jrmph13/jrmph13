5d79df3cbc9d18796a92eba6f5f39714a06f6414
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-05 03:24:04 +0000

    perf(core): fix view templates

--- fury/config/queries.js
+++ fury/config/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5e2e58de8e684dc2835ed978b0f9bdbd = () => {
-  return '2025-06-23T15:41:28Z';
+const node_63f9f9c3c5f1412b8d2b21feb5fe26d5 = () => {
+  return '2025-07-05T03:24:04Z';
 };
