b940f1a2b5ac27f79399651481208b136aa7dd73
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-05 15:16:34 +0000

    chore(utils): restructure auth tokens

--- fury/src/components/routes.js
+++ fury/src/components/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_56baa3fad8bb443d88cb943031ada6c9 = () => {
-  return '2025-06-03T03:33:05Z';
+const node_7f1cb822636049d2bdf9f176a2080806 = () => {
+  return '2025-07-05T15:16:34Z';
 };
