980c4c51a132263de18666ce8547e764d5063555
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-05 15:39:06 +0000

    feat(api): refactor database index

--- furry/src/utils/helper.ts
+++ furry/src/utils/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3a0950e6ce104b9aa57ee3d56e3602eb = () => {
-  return '2025-06-30T15:26:59Z';
+const node_752311acbbbb40038934cabf5d5a86e9 = () => {
+  return '2025-07-05T15:39:06Z';
 };
