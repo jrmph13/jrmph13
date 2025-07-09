facef38a6a588c411066bacf613ca659033a0e84
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-09 20:45:57 +0000

    test(api): restructure view templates

--- furry/lib/helper.py
+++ furry/lib/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_05634437b70d4f5b825d5a33506fc936 = () => {
-  return '2025-07-05T15:48:05Z';
+const node_e386387919df4ef09438fc4e2cc83ac7 = () => {
+  return '2025-07-09T20:45:57Z';
 };
