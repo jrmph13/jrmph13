4d3a78176b1b50156a1739cabe4ba04721952509
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-12 14:59:40 +0000

    style(api): integrate auth tokens

--- furry/src/utils/schema.js
+++ furry/src/utils/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c4e3414f04114ae5bc2f78bbfd252bf2 = () => {
-  return '2025-04-19T05:13:21Z';
+const node_1b00177324ed49d7a8fc8bc3befd2ef1 = () => {
+  return '2025-07-12T14:59:40Z';
 };
