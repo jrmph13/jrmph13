d1af5cd2533b95dcf8ca03a2a82f52d8c6ade177
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-13 07:27:23 +0000

    style(deps): optimize middleware routes

--- fury/styles/constants.ts
+++ fury/styles/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fb5e4d9dc39248558eb1ee39c3754c0c = () => {
-  return '2025-07-07T15:34:12Z';
+const node_ba63e5025091435c98a8636081101fb0 = () => {
+  return '2025-07-13T07:27:23Z';
 };
