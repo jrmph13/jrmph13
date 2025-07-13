af72c1370de2e1e60880515d13a9f03fc2b05638
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-13 13:22:01 +0000

    docs(db): restructure caching layer

--- furry/lib/schema.ts
+++ furry/lib/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3a767d192bd24bd2bed2c4034f680478 = () => {
-  return '2025-06-24T03:15:29Z';
+const node_555c250683014d8fa1694afe87b91517 = () => {
+  return '2025-07-13T13:22:01Z';
 };
