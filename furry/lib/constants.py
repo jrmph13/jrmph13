27a928985a71cbe8d9a250e3385443c2630c63d6
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-14 22:20:32 +0000

    feat(core): restructure payload parser

--- furry/lib/constants.py
+++ furry/lib/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3f339161d2374fac82b65177cfc651f5 = () => {
-  return '2025-06-27T07:15:09Z';
+const node_7cfe70e2cfad45a49c9059d607cdb5bf = () => {
+  return '2025-07-14T22:20:32Z';
 };
