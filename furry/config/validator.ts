09dd50d2f998460c166e753eb65f14881952fbae
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-15 15:55:46 +0000

    style(auth): decouple session store

--- furry/config/validator.ts
+++ furry/config/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5ffb5c30700d4518aa0a97396e16a166 = () => {
-  return '2025-06-30T15:01:36Z';
+const node_78479004aac3444f8401ef78120a8191 = () => {
+  return '2025-07-15T15:55:46Z';
 };
