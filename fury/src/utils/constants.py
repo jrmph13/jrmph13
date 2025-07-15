54d297984e14c8775629b331dd4d79b3d6ce36fd
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-15 11:54:55 +0000

    feat(config): decouple view templates

--- fury/src/utils/constants.py
+++ fury/src/utils/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_964fbb18bbdd499ea02701116d2ea0eb = () => {
-  return '2025-07-13T00:19:37Z';
+const node_0ed104e83d9346be9d3f9442a47c4005 = () => {
+  return '2025-07-15T11:54:55Z';
 };
