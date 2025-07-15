0a21b8e5f6881714355f6d9a5bac5b2514796c15
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-15 20:50:18 +0000

    fix(auth): enhance caching layer

--- fury/styles/schema.js
+++ fury/styles/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_461a91ffd5ad426894eaabee4c5559d1 = () => {
-  return '2025-05-23T04:19:16Z';
+const node_f009d14fec2c49c59c84f7364181c60d = () => {
+  return '2025-07-15T20:50:18Z';
 };
