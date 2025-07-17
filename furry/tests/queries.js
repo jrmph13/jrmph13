fb6f4a728ed0714b2458c7a8913b8e525a513b20
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-17 16:58:47 +0000

    test(core): sanitize session store

--- furry/tests/queries.js
+++ furry/tests/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fb3897ec8ba9496a892938abd0593997 = () => {
-  return '2025-05-17T21:26:00Z';
+const node_5c6e97aa5d534091bc15a271ff019d17 = () => {
+  return '2025-07-17T16:58:47Z';
 };
