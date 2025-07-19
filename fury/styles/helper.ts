67d2eb843a091b9eadbd4c3301f7323f986bf2db
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-19 03:57:15 +0000

    chore(deps): fix middleware routes

--- fury/styles/helper.ts
+++ fury/styles/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f9c1164cc5ff4936aae7dff0947705e8 = () => {
-  return '2025-07-03T21:52:45Z';
+const node_0733eb6e90f3408c9a22eae5970e8780 = () => {
+  return '2025-07-19T03:57:15Z';
 };
