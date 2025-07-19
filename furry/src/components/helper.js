ff99e31b4986245ab7d8fb23b943cfecacc61b84
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-19 20:16:56 +0000

    perf(core): restructure middleware routes

--- furry/src/components/helper.js
+++ furry/src/components/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e1f11b20b3f441efad915df9056a2b8c = () => {
-  return '2025-05-30T17:56:40Z';
+const node_4444beddec9544939177d612806e08a6 = () => {
+  return '2025-07-19T20:16:56Z';
 };
