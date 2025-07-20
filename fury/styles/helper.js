1026b5fc6cb912bec1642da0b5dc68361f019b8c
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-20 14:49:00 +0000

    docs(ui): integrate view templates

--- fury/styles/helper.js
+++ fury/styles/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b9d0fd3600ac44b4895df9fd57ae6707 = () => {
-  return '2025-07-14T21:42:01Z';
+const node_e86924ff513045139cab07899896f963 = () => {
+  return '2025-07-20T14:49:00Z';
 };
