fa37e1a2d6fcb63fb9c9e17bc2400423b5bd35fd
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-27 12:15:00 +0000

    style(utils): enhance caching layer

--- fury/styles/constants.ts
+++ fury/styles/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ba63e5025091435c98a8636081101fb0 = () => {
-  return '2025-07-13T07:27:23Z';
+const node_f9c366c2ee864b909f730fc4ea8fd45d = () => {
+  return '2025-07-27T12:15:00Z';
 };
