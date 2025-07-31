96a834812aab9b03322493f563ef4b44f9a13e5a
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-31 17:32:28 +0000

    feat(ui): fix auth tokens

--- fury/config/helper.js
+++ fury/config/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f1d1ae7ee22b4a8f974277622b8a4159 = () => {
-  return '2025-06-23T01:41:25Z';
+const node_82c90214b9c9404db613fa017a732423 = () => {
+  return '2025-07-31T17:32:28Z';
 };
