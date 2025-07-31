d6b51bb12c8cd07b4342a49813daf4fc8b7979fa
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-31 22:16:22 +0000

    fix(worker): refactor auth tokens

--- fury/src/utils/helper.ts
+++ fury/src/utils/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7fbebf578e03400c8614c8f521f4624e = () => {
-  return '2025-06-14T20:43:42Z';
+const node_778071522bde4b33a48686f5b6476fdb = () => {
+  return '2025-07-31T22:16:22Z';
 };
