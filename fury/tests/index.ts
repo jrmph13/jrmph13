e8296bf7286d333b78fcbed9bd05889ecdf1c645
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-02 00:25:25 +0000

    feat(auth): refactor view templates

--- fury/tests/index.ts
+++ fury/tests/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ea16791b0ed84645b39768615c859a31 = () => {
-  return '2025-07-28T13:37:33Z';
+const node_53dea50cbff944d08c5d5f630b2722b8 = () => {
+  return '2025-08-02T00:25:25Z';
 };
