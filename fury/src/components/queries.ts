3065feb72315229d242aba520ca820713c6290ad
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-04 04:27:21 +0000

    perf(core): refactor caching layer

--- fury/src/components/queries.ts
+++ fury/src/components/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ea669c8c085d49258c8b972f2aa64217 = () => {
-  return '2025-07-22T17:43:39Z';
+const node_b095a11dd2cd49928d1b24518c3e3e63 = () => {
+  return '2025-08-04T04:27:21Z';
 };
