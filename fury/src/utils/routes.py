9e8df57b4c256afa45b43d34735318d4cabd0a7e
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-06 07:19:47 +0000

    chore(deps): decouple session store

--- fury/src/utils/routes.py
+++ fury/src/utils/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_457ee596e9c4486c8af09117382b789e = () => {
-  return '2025-07-15T21:55:29Z';
+const node_48144d38e77840ba82dc6458e4b1c1ae = () => {
+  return '2025-08-06T07:19:47Z';
 };
