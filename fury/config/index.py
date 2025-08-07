0547c5402aa09e4606b630bac197f4fa70ebea36
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-07 23:35:01 +0000

    style(utils): enhance middleware routes

--- fury/config/index.py
+++ fury/config/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_eb41b70e5543439e84b0b515bd88c36d = () => {
-  return '2025-07-31T08:52:44Z';
+const node_cc57aaa69ba84e268d8bae7321391c63 = () => {
+  return '2025-08-07T23:35:01Z';
 };
