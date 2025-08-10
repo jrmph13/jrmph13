bd55b946de4cc593644b30bb6404a4bac7117303
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-10 20:05:03 +0000

    test(core): decouple logging interface

--- furry/config/routes.js
+++ furry/config/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8d162824c70b4b259be6a5f6734ba552 = () => {
-  return '2025-07-26T03:38:57Z';
+const node_582988d83a8d4bb8984667bbc5c79a0a = () => {
+  return '2025-08-10T20:05:03Z';
 };
