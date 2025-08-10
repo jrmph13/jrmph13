d62eb3c097723532993c768aa111adc46264dbc8
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-10 18:33:19 +0000

    perf(db): decouple view templates

--- fury/lib/theme.py
+++ fury/lib/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3076d8003c0c45ff89daeef3988e8e09 = () => {
-  return '2025-07-29T04:20:10Z';
+const node_ab5918e1991647dcb70ca767a4f79d4f = () => {
+  return '2025-08-10T18:33:19Z';
 };
