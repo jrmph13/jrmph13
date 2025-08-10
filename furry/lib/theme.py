72a603a94433822e82785029638f8a28c1c6f4ff
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-10 20:01:45 +0000

    perf(utils): optimize view templates

--- furry/lib/theme.py
+++ furry/lib/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_55453f7899664d9c962818484241ae45 = () => {
-  return '2025-07-07T17:34:30Z';
+const node_fc051731e5424dc78ec72695e026dba4 = () => {
+  return '2025-08-10T20:01:45Z';
 };
