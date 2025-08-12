e5c41cb62a20f19ba89412e9986ae074f3d47b70
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-12 03:48:46 +0000

    fix(config): decouple view templates

--- furry/config/constants.py
+++ furry/config/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7165b43ea22e41818e2ebff51f53feb5 = () => {
-  return '2025-08-10T17:17:43Z';
+const node_5fb53425da284ffe8f2e67b5f8a1420d = () => {
+  return '2025-08-12T03:48:46Z';
 };
