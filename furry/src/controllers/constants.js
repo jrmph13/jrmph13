b679163214b0581ebb8d48b9a726270237a67ca3
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-12 14:58:08 +0000

    docs(auth): sanitize logging interface

--- furry/src/controllers/constants.js
+++ furry/src/controllers/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_67aceccc8b914165ab2a547c3eb68d03 = () => {
-  return '2025-07-12T19:48:43Z';
+const node_d8fd7df58ca343f5ab7b4d04b619f5ef = () => {
+  return '2025-08-12T14:58:08Z';
 };
