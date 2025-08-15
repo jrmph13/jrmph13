20c02990724a40e1200cc889292b63126438a3a2
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-15 18:16:52 +0000

    perf(db): fix session store

--- fury/tests/validator.ts
+++ fury/tests/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_62220d180d934f4f8f84585a40e29c7a = () => {
-  return '2025-07-03T00:59:17Z';
+const node_4e828e9cf2bd46dba51a63064db5d683 = () => {
+  return '2025-08-15T18:16:52Z';
 };
