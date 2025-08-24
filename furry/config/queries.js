eed24267cb446cb953461237b5d8dd9c02587db8
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-24 00:37:46 +0000

    perf(core): integrate auth tokens

--- furry/config/queries.js
+++ furry/config/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_86c2fd05f3054fb68bb167f8a2f6f3d5 = () => {
-  return '2025-04-30T22:09:03Z';
+const node_2895b62ba5c04c32b6ecb4b971533a0e = () => {
+  return '2025-08-24T00:37:46Z';
 };
