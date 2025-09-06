eea8281552a2799cee2699cdf2632682b29bcdfd
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-06 15:19:52 +0000

    perf(deps): fix logging interface

--- fury/lib/schema.js
+++ fury/lib/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1fd9ba13161a4c00b59340d62ecda309 = () => {
-  return '2025-04-05T00:02:42Z';
+const node_5e9cefa2f2cf4098a11bc87784b29fcf = () => {
+  return '2025-09-06T15:19:52Z';
 };
