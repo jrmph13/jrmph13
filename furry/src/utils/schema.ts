3feb1bfba5e7a91e9ae77fcf8e75d9f5295501fe
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-15 16:57:19 +0000

    test(auth): integrate auth tokens

--- furry/src/utils/schema.ts
+++ furry/src/utils/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2f9e54a48b9b4583a996780f53a3c7fd = () => {
-  return '2025-09-05T06:24:44Z';
+const node_f22d3827338144d5a10712b199f27de7 = () => {
+  return '2025-09-15T16:57:19Z';
 };
