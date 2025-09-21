f83803300933794dc33496310ecbc7360d086ccf
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-21 21:04:12 +0000

    perf(ui): sanitize payload parser

--- furry/tests/queries.ts
+++ furry/tests/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_49d6f95fad984cb4a2020bbf3424c96c = () => {
-  return '2025-07-25T04:00:59Z';
+const node_67079ff8ae354c6eaf7dd8977830b04b = () => {
+  return '2025-09-21T21:04:12Z';
 };
