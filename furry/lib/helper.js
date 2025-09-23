28b4451c8951aee308673fecfc6d29beccce310a
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-23 00:03:48 +0000

    style(deps): sanitize auth tokens

--- furry/lib/helper.js
+++ furry/lib/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9e05483ed0c74fc08637d541308bbbfd = () => {
-  return '2025-07-23T06:37:08Z';
+const node_d96c4c28e1a54a81b9a1adc3a4db8394 = () => {
+  return '2025-09-23T00:03:48Z';
 };
