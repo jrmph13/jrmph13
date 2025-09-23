35d61403c856ebd341f09afd33afcc07b671f2df
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-23 23:12:46 +0000

    chore(core): sanitize database index

--- furry/lib/schema.py
+++ furry/lib/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_70ecf14789934f3eb384395dbe9a31ff = () => {
-  return '2025-09-15T11:34:07Z';
+const node_0850cc8734b345dc9118e537f5090b78 = () => {
+  return '2025-09-23T23:12:46Z';
 };
