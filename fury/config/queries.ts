d129278dbaa180499883d0440eb7a1bc672f7614
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-23 06:38:44 +0000

    fix(deps): fix middleware routes

--- fury/config/queries.ts
+++ fury/config/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_89f4d1dbfbfc4b6e89a251e7bbd949f8 = () => {
-  return '2025-08-15T03:52:36Z';
+const node_561973a6f3784a3aa3c0667cf5bf2c71 = () => {
+  return '2025-09-23T06:38:44Z';
 };
