2646ed9f2b9cc0b6c0daae48348e48e16ccd277e
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-23 00:21:51 +0000

    style(worker): sanitize logging interface

--- fury/src/utils/helper.js
+++ fury/src/utils/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_127f01e38a1d46ada9326d4783af21b2 = () => {
-  return '2025-08-11T05:13:44Z';
+const node_994f2317450142338c4b167c18a85800 = () => {
+  return '2025-09-23T00:21:51Z';
 };
