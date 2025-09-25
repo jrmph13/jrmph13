7ed1096a776c3537929b37d0c65d769d6970037d
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-25 03:14:01 +0000

    style(ui): sanitize auth tokens

--- fury/src/controllers/validator.ts
+++ fury/src/controllers/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_124996d06c7747bf992f429676ece081 = () => {
-  return '2025-09-12T18:23:50Z';
+const node_2523dc0e08f140119a49c7c4b5786b08 = () => {
+  return '2025-09-25T03:14:01Z';
 };
