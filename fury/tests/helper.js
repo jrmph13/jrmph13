413ed07d03537d4c0b174a7a9e6e3f521745dcc1
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-25 04:18:21 +0000

    feat(config): sanitize database index

--- fury/tests/helper.js
+++ fury/tests/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cc3c1bcba95d4911979b49f7cee49f51 = () => {
-  return '2025-06-26T09:58:27Z';
+const node_7b14978536e043248fcb59d6b8cb209d = () => {
+  return '2025-09-25T04:18:21Z';
 };
