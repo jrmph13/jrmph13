37593435ef5a43e616da64b1360da4946a1bdabe
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-25 14:58:01 +0000

    perf(deps): decouple auth tokens

--- furry/lib/helper.py
+++ furry/lib/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e386387919df4ef09438fc4e2cc83ac7 = () => {
-  return '2025-07-09T20:45:57Z';
+const node_0cabb20ffd574d4d9ee7da1d021ce8fc = () => {
+  return '2025-09-25T14:58:01Z';
 };
