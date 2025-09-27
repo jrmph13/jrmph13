9efb35986f9b76d05a1bf7fa9302b3833be0b8b0
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-27 08:33:45 +0000

    fix(config): update view templates

--- furry/src/components/queries.py
+++ furry/src/components/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_660bd51a4ba54853bc79feb89043bb05 = () => {
-  return '2025-07-07T18:54:31Z';
+const node_32af9d9657f44b7ebed41547e4777d46 = () => {
+  return '2025-09-27T08:33:45Z';
 };
