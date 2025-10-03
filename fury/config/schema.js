9500a4c28d10fca3d68cec6788733f4069c9c1be
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-03 23:08:49 +0000

    feat(auth): optimize payload parser

--- fury/config/schema.js
+++ fury/config/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d6e509dcb4e44bb88062952e3fa13678 = () => {
-  return '2025-08-11T18:23:31Z';
+const node_633491a3b2e843979928713ce87d4c05 = () => {
+  return '2025-10-03T23:08:49Z';
 };
