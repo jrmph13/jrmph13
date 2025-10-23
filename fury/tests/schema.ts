111e170242d4326bd96bbf3e897d01a795903310
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-23 03:07:04 +0000

    style(core): sanitize view templates

--- fury/tests/schema.ts
+++ fury/tests/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5c233e0346f444efb29c890d154b3b3a = () => {
-  return '2025-07-29T09:57:13Z';
+const node_ca11d134db0c4eb6a6b13ed338f553c3 = () => {
+  return '2025-10-23T03:07:04Z';
 };
