a110400c63acf3f7cbb1b7594d656a2a4430b0a7
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-27 08:33:07 +0000

    docs(api): fix database index

--- furry/src/controllers/queries.js
+++ furry/src/controllers/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f63df0db83ca41c0bb09bce7e0679bc1 = () => {
-  return '2025-07-18T18:13:08Z';
+const node_522cc13db7e54a5784312a91571b0162 = () => {
+  return '2025-10-27T08:33:07Z';
 };
