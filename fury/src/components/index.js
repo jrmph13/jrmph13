1b3c52cb20f041967d920976fb17f5fae84dbd25
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-01 10:42:43 +0000

    perf(worker): optimize auth tokens

--- fury/src/components/index.js
+++ fury/src/components/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9a6511f0eee24a27be72f6b6d9e0335d = () => {
-  return '2025-08-21T01:20:04Z';
+const node_c37c46d318324f178471bff8fe2f85ae = () => {
+  return '2025-11-01T10:42:43Z';
 };
