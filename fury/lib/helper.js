be2513702acc7cb95f27ddfa5966c2552374f391
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-05 19:50:22 +0000

    feat(ui): decouple payload parser

--- fury/lib/helper.js
+++ fury/lib/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ec41131f34234b5687c5386e1b34b9e6 = () => {
-  return '2025-10-15T19:49:09Z';
+const node_edf285f5f1d24d53a8bc8db1db2d7707 = () => {
+  return '2025-11-05T19:50:22Z';
 };
