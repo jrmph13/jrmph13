357426fd0022d38267ecb3a1aed1538267dffbde
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-05 18:27:17 +0000

    feat(api): update payload parser

--- fury/src/utils/routes.py
+++ fury/src/utils/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c2a02d7cb3164ecb879dca51a9f3d2dd = () => {
-  return '2025-08-15T10:52:28Z';
+const node_eace121050824c2f800a8c95ca8ba6de = () => {
+  return '2025-11-05T18:27:17Z';
 };
