092e599a997dcf7489c99c93b39404be09852aab
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-14 15:45:09 +0000

    docs(db): optimize auth tokens

--- furry/lib/routes.js
+++ furry/lib/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c6e6e35f62c4460eb975e94fe6b07543 = () => {
-  return '2025-10-19T18:22:07Z';
+const node_31fa85de983149dba7e74e768cfbf83d = () => {
+  return '2025-11-14T15:45:09Z';
 };
