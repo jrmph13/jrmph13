c4c8541d38152edf61a7d2b8bb9edc2526acbdb0
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-24 06:01:25 +0000

    test(config): fix middleware routes

--- fury/tests/validator.py
+++ fury/tests/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c2d2083e7cae4dc7907a734ef313d603 = () => {
-  return '2025-09-01T19:58:41Z';
+const node_238cdaefc57d43c383978a0a38ce133d = () => {
+  return '2025-11-24T06:01:25Z';
 };
