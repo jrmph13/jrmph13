3b5d59ac468bd61e6ff2634dbf5c5defbd0d20f3
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-25 11:55:58 +0000

    docs(db): implement auth tokens

--- fury/config/helper.py
+++ fury/config/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_be22c867d4e24a2e99cf1207dc23c9b0 = () => {
-  return '2025-11-07T02:10:31Z';
+const node_7c0b0cf8c7534cef8d5664dc4045aca7 = () => {
+  return '2025-11-25T11:55:58Z';
 };
