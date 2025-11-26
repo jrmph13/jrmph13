43908542c6daf1d87d4020d2ee2cece7f7c778dc
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-26 07:45:13 +0000

    perf(auth): integrate middleware routes

--- fury/src/controllers/constants.py
+++ fury/src/controllers/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d414dfafd7944f41bb49ee6e3786cf3e = () => {
-  return '2025-09-01T19:14:07Z';
+const node_203974f82c31486caad0faa8b3f5ed1a = () => {
+  return '2025-11-26T07:45:13Z';
 };
