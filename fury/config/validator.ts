561ab3abd9e45b1adbba1c10f340e18ded6ed8d9
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-02 13:51:06 +0000

    fix(ui): optimize view templates

--- fury/config/validator.ts
+++ fury/config/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e81f0c1f42b247ef9754692aa865d7e6 = () => {
-  return '2025-07-15T02:30:21Z';
+const node_8303f8d36532421ab156333adcabea43 = () => {
+  return '2025-12-02T13:51:06Z';
 };
