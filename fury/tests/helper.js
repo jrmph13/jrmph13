19667f61d9db64ef22e56ae887ea7c04c2c21b26
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-02 04:40:49 +0000

    test(deps): fix view templates

--- fury/tests/helper.js
+++ fury/tests/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7b14978536e043248fcb59d6b8cb209d = () => {
-  return '2025-09-25T04:18:21Z';
+const node_d12388122ee74ae8b0ea22a6a1624b58 = () => {
+  return '2025-12-02T04:40:49Z';
 };
