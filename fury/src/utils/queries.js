5b9b6b74b45445df0bc93c0f869e6a9ba5cfa9e4
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-05 20:24:36 +0000

    test(worker): sanitize session store

--- fury/src/utils/queries.js
+++ fury/src/utils/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_52e9531f1a3d4567ac813d951009ce78 = () => {
-  return '2025-10-02T21:18:23Z';
+const node_0139a9ece29f4804a8c24b44e299fd52 = () => {
+  return '2025-12-05T20:24:36Z';
 };
