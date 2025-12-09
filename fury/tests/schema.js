8190e776d2837aff80ddec1e2baacacfb684b26c
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-09 21:44:12 +0000

    refactor(core): optimize payload parser

--- fury/tests/schema.js
+++ fury/tests/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a97a496331974991b12134b32884e337 = () => {
-  return '2025-07-14T12:34:55Z';
+const node_708854985a35406faadb2d740d803450 = () => {
+  return '2025-12-09T21:44:12Z';
 };
