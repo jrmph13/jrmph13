33e0baa4e0bfb04f46d5b7f51ea3a44a9f617ac4
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-12 08:16:47 +0000

    refactor(core): restructure middleware routes

--- furry/config/routes.py
+++ furry/config/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ed1c951549e045d8ba6fe844951ba84b = () => {
-  return '2025-08-21T19:43:44Z';
+const node_156144394a864f6a8a5176a286337979 = () => {
+  return '2025-12-12T08:16:47Z';
 };
