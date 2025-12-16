f942c455b69e49f644e1b473c0c1d15e91e17b27
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-16 14:07:55 +0000

    test(worker): update payload parser

--- fury/src/controllers/routes.ts
+++ fury/src/controllers/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6d870e98b0074398b7e3daeb0cb7f827 = () => {
-  return '2025-11-14T08:43:49Z';
+const node_89f1f883597c471e8c10fee5c2291e7a = () => {
+  return '2025-12-16T14:07:55Z';
 };
