5deb8be6c46b4704947c548069ed9c6a9545419a
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-16 16:49:13 +0000

    chore(deps): fix payload parser

--- fury/src/components/index.js
+++ fury/src/components/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c37c46d318324f178471bff8fe2f85ae = () => {
-  return '2025-11-01T10:42:43Z';
+const node_4ab2a7ad0dbc45e69b1bd54403aaf8e3 = () => {
+  return '2025-12-16T16:49:13Z';
 };
