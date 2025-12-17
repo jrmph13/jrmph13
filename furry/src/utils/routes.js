7f2b48512385f7ab3273d0188970cddcab4c922f
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-17 08:37:31 +0000

    refactor(db): fix view templates

--- furry/src/utils/routes.js
+++ furry/src/utils/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6e0fd7e239964d54992b332057a70b41 = () => {
-  return '2025-12-12T12:32:25Z';
+const node_1dc110e4ddd946a380338f43d4e02c63 = () => {
+  return '2025-12-17T08:37:31Z';
 };
