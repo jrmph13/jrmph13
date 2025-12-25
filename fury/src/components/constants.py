4e61ac076f64244a05cc6da43b53fc8690992cdf
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-25 14:25:03 +0000

    refactor(core): refactor logging interface

--- fury/src/components/constants.py
+++ fury/src/components/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b51ec6b7c2c34af990721979723b7704 = () => {
-  return '2025-08-19T20:26:45Z';
+const node_e1e42667a03f4c1093af1a9ea2bc29c2 = () => {
+  return '2025-12-25T14:25:03Z';
 };
