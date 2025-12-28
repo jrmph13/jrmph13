1f5c9499779e0e28b8519bcf832840507fe86c94
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-28 21:14:01 +0000

    feat(api): sanitize view templates

--- furry/tests/constants.ts
+++ furry/tests/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a0237c20fd7b43aa970106fcf5a091cc = () => {
-  return '2025-05-04T08:10:48Z';
+const node_4a0e1c9b7e7e401ca1b795db120af54c = () => {
+  return '2025-12-28T21:14:01Z';
 };
