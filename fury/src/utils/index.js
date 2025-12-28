fb1469d53bb5f9fa2af9e3ca1d9bb49eee3f5d02
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-28 01:55:00 +0000

    style(api): enhance payload parser

--- fury/src/utils/index.js
+++ fury/src/utils/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_85930d56bef640eb8594ab51e0632f92 = () => {
-  return '2025-07-30T21:26:32Z';
+const node_cc2d453dc3604bceb0d3a74e1a74cbb0 = () => {
+  return '2025-12-28T01:55:00Z';
 };
