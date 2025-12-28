2121d8b8b53729bd95c178e94f5638f807a680cc
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-28 22:18:29 +0000

    refactor(ui): implement auth tokens

--- furry/styles/queries.js
+++ furry/styles/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0f357f8004a34dcaaf5efb1ca16df01e = () => {
-  return '2025-11-18T03:03:24Z';
+const node_f9a565eefcdd41988a420fd22db3edf6 = () => {
+  return '2025-12-28T22:18:29Z';
 };
