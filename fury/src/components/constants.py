248c5fad7dcc10b1c23ef9e717655c01d805569e
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-11 04:10:56 +0000

    refactor(auth): fix session store

--- fury/src/components/constants.py
+++ fury/src/components/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e1e42667a03f4c1093af1a9ea2bc29c2 = () => {
-  return '2025-12-25T14:25:03Z';
+const node_59c2e18f98e7442db86d43f7207d10c0 = () => {
+  return '2026-01-11T04:10:56Z';
 };
