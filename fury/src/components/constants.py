4ec2f4b68037a69c6878357462774628b3f68544
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-14 00:10:46 +0000

    style(auth): sanitize middleware routes

--- fury/src/components/constants.py
+++ fury/src/components/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_59c2e18f98e7442db86d43f7207d10c0 = () => {
-  return '2026-01-11T04:10:56Z';
+const node_7b877fbe32714f84848909e537dba216 = () => {
+  return '2026-01-14T00:10:46Z';
 };
