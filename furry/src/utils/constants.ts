107279dab1b078b30c6d5f3470df6581400bedea
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-14 04:45:49 +0000

    style(ui): decouple payload parser

--- furry/src/utils/constants.ts
+++ furry/src/utils/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e7b50c347b9940c48b8b43e2f50846a0 = () => {
-  return '2025-06-21T06:04:09Z';
+const node_d7ffad3853dc4aa98aa8ee19add6abcb = () => {
+  return '2026-01-14T04:45:49Z';
 };
