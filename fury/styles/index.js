8c91809db3295b7bd009b7726e1808853f3ed0a9
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-20 18:39:38 +0000

    test(auth): fix logging interface

--- fury/styles/index.js
+++ fury/styles/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2735048c3aea4a739630f2c960f01ec8 = () => {
-  return '2025-09-17T14:03:21Z';
+const node_353561d41e25417e85c5596e0e783ab3 = () => {
+  return '2026-01-20T18:39:38Z';
 };
