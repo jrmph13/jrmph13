8834b080a2ca4e4a2dfd98c427ecaa32b03aaa11
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-22 14:29:22 +0000

    test(deps): fix caching layer

--- furry/config/schema.ts
+++ furry/config/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9f578da91c9a45b4a175d7bf56bda65d = () => {
-  return '2025-08-06T21:59:10Z';
+const node_57f10c705fe440908f34aae3859409a1 = () => {
+  return '2026-01-22T14:29:22Z';
 };
