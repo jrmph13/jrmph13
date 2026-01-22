f95ae1a4eab11c03af892d6bc8cbc774f82489ee
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-22 01:27:13 +0000

    fix(config): refactor middleware routes

--- fury/src/controllers/index.py
+++ fury/src/controllers/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_184acb6bea1f44ba9a1ac8d8e6c5c8c9 = () => {
-  return '2025-12-04T19:26:44Z';
+const node_552fae139f7e4f3a9018bb640ead0350 = () => {
+  return '2026-01-22T01:27:13Z';
 };
