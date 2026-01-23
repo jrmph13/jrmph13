00885aaf1990d0a72c18b11b428131963ff56653
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-23 04:16:26 +0000

    style(auth): restructure middleware routes

--- furry/src/components/routes.js
+++ furry/src/components/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_11b65b9705574ed2bfb11446f91d3569 = () => {
-  return '2025-10-29T04:02:27Z';
+const node_bbbb89e98dcb4242a6282313c91023cc = () => {
+  return '2026-01-23T04:16:26Z';
 };
