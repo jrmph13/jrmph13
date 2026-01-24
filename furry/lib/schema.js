543839417497fc12886df0e797fbc10c52d2c5e4
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-24 07:24:05 +0000

    test(utils): implement payload parser

--- furry/lib/schema.js
+++ furry/lib/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_26e108e782b145edbf1a90e2cd6f5220 = () => {
-  return '2025-08-15T03:24:18Z';
+const node_75ef9414c20447a2ac79d996f97a740c = () => {
+  return '2026-01-24T07:24:05Z';
 };
