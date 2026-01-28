4932ebb35ea860f3a764214a251f60e54c55ecc1
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-28 20:14:14 +0000

    perf(ui): integrate payload parser

--- fury/config/routes.js
+++ fury/config/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c1da949e10844708852897e282057f93 = () => {
-  return '2025-06-11T03:17:33Z';
+const node_2819e53ca99a4fcaa6875f7e49f13cf1 = () => {
+  return '2026-01-28T20:14:14Z';
 };
