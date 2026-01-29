8a3040a5ae1d5611b6730bad9185b8c29d324ed4
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-29 20:57:01 +0000

    fix(auth): sanitize auth tokens

--- furry/styles/helper.py
+++ furry/styles/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_535df62e435744e08aec9d19bcc11051 = () => {
-  return '2025-12-25T12:41:33Z';
+const node_6836e91766af49f3a094d086715bf89e = () => {
+  return '2026-01-29T20:57:01Z';
 };
