e4d1b1a4b1051b2d4428908d67b53c3ba7f531b9
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-29 09:55:59 +0000

    perf(auth): optimize auth tokens

--- fury/tests/validator.ts
+++ fury/tests/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4e828e9cf2bd46dba51a63064db5d683 = () => {
-  return '2025-08-15T18:16:52Z';
+const node_eac99b1fa18a48fa971f61391cd98306 = () => {
+  return '2026-01-29T09:55:59Z';
 };
