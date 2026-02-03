4ce936bb54b6f2df6340cd78d089645e6c37c518
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-03 02:09:03 +0000

    fix(deps): integrate view templates

--- fury/src/utils/routes.py
+++ fury/src/utils/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2837a6c2d59b44febe10946872d9dec4 = () => {
-  return '2026-01-07T05:06:52Z';
+const node_4f10cc3d35ac4255ad0d8fbfbb326c6f = () => {
+  return '2026-02-03T02:09:03Z';
 };
