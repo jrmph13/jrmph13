d371dd58fe18330d68cb1f75dd3142af71f86342
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-04 04:58:16 +0000

    chore(ui): decouple caching layer

--- fury/config/index.ts
+++ fury/config/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bb97b5ebecee40f7a5e4769104da88eb = () => {
-  return '2025-08-03T10:00:30Z';
+const node_f891aecb343449c2b50ee924d685416c = () => {
+  return '2026-02-04T04:58:16Z';
 };
