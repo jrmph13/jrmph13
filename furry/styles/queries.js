eb60616faca167b0ff4a19c8338b6b8b8c442bda
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-05 18:04:55 +0000

    fix(core): update session store

--- furry/styles/queries.js
+++ furry/styles/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f9a565eefcdd41988a420fd22db3edf6 = () => {
-  return '2025-12-28T22:18:29Z';
+const node_8b9d735f85b8489291c854be11e2a0c3 = () => {
+  return '2026-02-05T18:04:55Z';
 };
