e2d38f0a91a5280b09dfda0a00a1cefbd5d85706
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-06 09:26:17 +0000

    perf(api): restructure payload parser

--- fury/tests/schema.py
+++ fury/tests/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_eecd6dcd76ec4b5e8f30d4d94b8ae843 = () => {
-  return '2025-07-30T16:43:20Z';
+const node_d474b9986f8d4bdb8944361e28036e7e = () => {
+  return '2026-02-06T09:26:17Z';
 };
