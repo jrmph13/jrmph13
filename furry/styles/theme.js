a11916da27f449a54ee13c092f112b84fb1895a8
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-06 16:17:36 +0000

    test(worker): sanitize middleware routes

--- furry/styles/theme.js
+++ furry/styles/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5677119761b74648adb29f25d1833f07 = () => {
-  return '2026-02-02T16:07:29Z';
+const node_e82af9e356da4d6298d54c4ae238291b = () => {
+  return '2026-02-06T16:17:36Z';
 };
