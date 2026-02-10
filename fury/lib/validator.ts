58f61e2fbbd95c689407e0823c4de16cc6f77584
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-10 02:08:26 +0000

    fix(worker): implement middleware routes

--- fury/lib/validator.ts
+++ fury/lib/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d2265de2c8be451cb135af935295223f = () => {
-  return '2025-07-29T09:20:29Z';
+const node_d03e1cc7b2154bce85b5fb5db2704846 = () => {
+  return '2026-02-10T02:08:26Z';
 };
