75787cf99b7a30e17d7fa1b4be7218408d8f995c
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-10 16:19:26 +0000

    test(core): decouple auth tokens

--- furry/config/validator.ts
+++ furry/config/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_db971cbd47e845e2bcb69cea33cc428e = () => {
-  return '2026-01-23T14:52:22Z';
+const node_3e0b2dbcc8744866a730497da018e85a = () => {
+  return '2026-02-10T16:19:26Z';
 };
