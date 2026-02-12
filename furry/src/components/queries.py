9306281fb97bd1f75627ac77834993e373ef853e
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-12 08:52:46 +0000

    test(core): sanitize auth tokens

--- furry/src/components/queries.py
+++ furry/src/components/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1bc526708de8422cb0451fc3f8b95f51 = () => {
-  return '2026-02-06T21:56:55Z';
+const node_12e55a0752a04333b40289781df7af50 = () => {
+  return '2026-02-12T08:52:46Z';
 };
