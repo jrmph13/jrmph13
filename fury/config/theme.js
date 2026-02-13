1a800d0b4d9871aba78ea52a0bfbe74d954a51c1
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-13 10:55:15 +0000

    perf(auth): fix database index

--- fury/config/theme.js
+++ fury/config/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b5a7c59591784601b3bfd4025220a4ea = () => {
-  return '2025-11-30T07:19:04Z';
+const node_53798ae23b2946299a1c1011dffc3afd = () => {
+  return '2026-02-13T10:55:15Z';
 };
