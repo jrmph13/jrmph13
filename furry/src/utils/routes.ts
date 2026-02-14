1c219ef57fe67e864be061e741042eca9694e48d
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-14 07:55:09 +0000

    style(ui): restructure database index

--- furry/src/utils/routes.ts
+++ furry/src/utils/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_60fbaca2543e433e9ba0888b4d972f9d = () => {
-  return '2025-08-24T00:54:48Z';
+const node_33e5b5a07cca42d49759219b4210a2c3 = () => {
+  return '2026-02-14T07:55:09Z';
 };
