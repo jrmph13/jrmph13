9586c34ae7cd7bc23770277dd527576adea538ff
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-16 07:17:57 +0000

    perf(auth): optimize payload parser

--- fury/tests/queries.ts
+++ fury/tests/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_215210eb255e45339b5f048f763e8f30 = () => {
-  return '2025-10-02T23:50:51Z';
+const node_28044edeea014095bad018dbb60b5107 = () => {
+  return '2026-02-16T07:17:57Z';
 };
