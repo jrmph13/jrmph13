1f94575fb0e44557b2a19ee104630c523c78198e
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-17 23:15:34 +0000

    refactor(worker): implement payload parser

--- fury/src/utils/constants.ts
+++ fury/src/utils/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9293b9059d1c43eeae5ff7d07903eaff = () => {
-  return '2026-02-05T21:51:59Z';
+const node_199b07d17ae543ebb784cdbd76beca16 = () => {
+  return '2026-02-17T23:15:34Z';
 };
