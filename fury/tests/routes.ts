8e96f48d83eaa8a34637f17e38b203cb8fa2b397
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-19 13:36:23 +0000

    fix(core): update database index

--- fury/tests/routes.ts
+++ fury/tests/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7cb5a7ffee4a4d368180d27633b7d7f4 = () => {
-  return '2026-02-17T07:32:14Z';
+const node_46565906989748d58489579705e2ee93 = () => {
+  return '2026-02-19T13:36:23Z';
 };
