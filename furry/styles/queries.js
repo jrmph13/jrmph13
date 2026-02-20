139ea48a2be462c145867a40c40e1e7c9e72bbf2
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-20 11:50:32 +0000

    refactor(api): fix caching layer

--- furry/styles/queries.js
+++ furry/styles/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8b9d735f85b8489291c854be11e2a0c3 = () => {
-  return '2026-02-05T18:04:55Z';
+const node_1567a055809440edb9916932e381995a = () => {
+  return '2026-02-20T11:50:32Z';
 };
