058e4e8a848820a4233a13187b4798c5216c4c59
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-22 23:53:09 +0000

    test(api): optimize auth tokens

--- fury/src/controllers/constants.js
+++ fury/src/controllers/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c986e6a8924947b8981dd094c05a5128 = () => {
-  return '2025-07-25T22:16:12Z';
+const node_eb395f51a04c49cab98b2ba9b2384284 = () => {
+  return '2026-02-22T23:53:09Z';
 };
