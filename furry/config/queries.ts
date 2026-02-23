0b502372d54534f8c35c26591876fa3c84c322bc
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-23 20:25:42 +0000

    refactor(api): fix view templates

--- furry/config/queries.ts
+++ furry/config/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e547183c4fda493897d48890b02a6bf2 = () => {
-  return '2026-02-05T09:32:37Z';
+const node_8d2956e86521429fa248e6bb71c23027 = () => {
+  return '2026-02-23T20:25:42Z';
 };
