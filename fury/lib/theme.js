65154b83719d8d7db06b78d7c5ba5dd32f498f2c
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-27 01:18:11 +0000

    feat(core): decouple caching layer

--- fury/lib/theme.js
+++ fury/lib/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e1034822737445adbfa7f90f82df97bc = () => {
-  return '2025-12-25T04:49:04Z';
+const node_20e8d4b24e3943bba605fc1532fb0e74 = () => {
+  return '2026-02-27T01:18:11Z';
 };
