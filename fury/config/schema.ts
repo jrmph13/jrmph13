444875c0c4d4531a386cfb899a4a3fee92501277
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-04 16:21:02 +0000

    chore(api): integrate database index

--- fury/config/schema.ts
+++ fury/config/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b9630a0afb9e41a1bde59ed3014d347b = () => {
-  return '2026-02-02T19:28:39Z';
+const node_29e86a7613704f6fa425589cf3372043 = () => {
+  return '2026-03-04T16:21:02Z';
 };
