3de8fe47ae9ed7c075a3a9c1e3662776eaba01e0
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-08 10:25:25 +0000

    chore(core): enhance auth tokens

--- fury/src/controllers/validator.js
+++ fury/src/controllers/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7fa9d42509e1465296f8e8af1573bd86 = () => {
-  return '2026-02-14T04:26:38Z';
+const node_ee28d53a748d462c9bb58b38c656238e = () => {
+  return '2026-03-08T10:25:25Z';
 };
