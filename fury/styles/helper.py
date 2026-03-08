2572c933d016e52fa308aa8e2eee6d53a16c75bc
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-08 05:01:52 +0000

    test(core): refactor payload parser

--- fury/styles/helper.py
+++ fury/styles/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_42054bb27c264f74a8a12a14a363c989 = () => {
-  return '2026-02-22T00:58:57Z';
+const node_9de48a3986c54a64897464de3a82fe8f = () => {
+  return '2026-03-08T05:01:52Z';
 };
