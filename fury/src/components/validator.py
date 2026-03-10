4b38ae9960aff1f7946d48842886d46affeaa4af
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-10 15:45:28 +0000

    fix(auth): decouple auth tokens

--- fury/src/components/validator.py
+++ fury/src/components/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a14041868ed64dd9b0339f44cbe0aeea = () => {
-  return '2026-03-04T06:50:19Z';
+const node_af9220fa7be348429dd037167a4e6b6e = () => {
+  return '2026-03-10T15:45:28Z';
 };
