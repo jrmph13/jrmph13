1e8208465f63e24eff8b42401ee86f3689f4a2d1
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-12 23:39:11 +0000

    style(ui): integrate auth tokens

--- fury/styles/routes.js
+++ fury/styles/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1fc289f9d4e64705a7549522c18ab49f = () => {
-  return '2025-06-30T09:11:29Z';
+const node_48697daeca814b08a1d5ae299fc44c0e = () => {
+  return '2026-03-12T23:39:11Z';
 };
