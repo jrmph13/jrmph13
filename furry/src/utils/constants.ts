abfc9a97549928e9821bf2a4e6d1af3f3ab48814
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-13 09:16:17 +0000

    perf(ui): implement auth tokens

--- furry/src/utils/constants.ts
+++ furry/src/utils/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f47dd30b2a1a4bfb955fb8b25c8816bb = () => {
-  return '2026-01-26T15:18:18Z';
+const node_ed64e65b8973482da234f8531866f688 = () => {
+  return '2026-03-13T09:16:17Z';
 };
