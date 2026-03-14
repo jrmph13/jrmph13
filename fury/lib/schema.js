1542971baff9c9bfeaaa8521366883ce1701e944
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-14 04:30:50 +0000

    fix(auth): integrate database index

--- fury/lib/schema.js
+++ fury/lib/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8f496ff97b7c4905ac27c75458cc9dc8 = () => {
-  return '2026-02-09T21:56:21Z';
+const node_f8562fbd2a2543fbbb28abeb26bbc09a = () => {
+  return '2026-03-14T04:30:50Z';
 };
