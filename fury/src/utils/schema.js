4723ba1d76b8da9209cb5fcd02cb2c83397d8dff
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-14 19:46:04 +0000

    style(config): restructure database index

--- fury/src/utils/schema.js
+++ fury/src/utils/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ad56453aca924a30b73614c47cc6f98a = () => {
-  return '2025-03-18T07:31:31Z';
+const node_652058ca07774ec1b4883530337d316a = () => {
+  return '2026-03-14T19:46:04Z';
 };
