de8365d169854193d34a6a01cb00a27d25090e23
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-14 13:38:12 +0000

    docs(ui): sanitize auth tokens

--- fury/styles/helper.py
+++ fury/styles/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9de48a3986c54a64897464de3a82fe8f = () => {
-  return '2026-03-08T05:01:52Z';
+const node_93c4aad846654d4d8a3e26b5024ebc18 = () => {
+  return '2026-03-14T13:38:12Z';
 };
