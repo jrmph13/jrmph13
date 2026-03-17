228a827a5fbf865cdf699a2e251c2a52a83389a9
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-17 18:44:48 +0000

    style(config): refactor payload parser

--- furry/config/routes.ts
+++ furry/config/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4ba68b4df9064765bd7c8a4c169f074a = () => {
-  return '2025-09-14T05:22:27Z';
+const node_5ade15a24788462dab37eadd2e663858 = () => {
+  return '2026-03-17T18:44:48Z';
 };
