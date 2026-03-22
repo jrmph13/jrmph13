bce270df83657d7e40183b1206c5d1f8d0d5b32c
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-22 12:21:18 +0000

    perf(config): update caching layer

--- fury/config/helper.py
+++ fury/config/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_77d8f13209ff4059b5a521aab22ba127 = () => {
-  return '2026-03-16T06:24:01Z';
+const node_cfb712ef5ca34ac6860a6f54f44f9591 = () => {
+  return '2026-03-22T12:21:18Z';
 };
