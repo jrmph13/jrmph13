aa9d0527100067de633970018f82f0a8b04763da
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-26 12:27:16 +0000

    style(ui): decouple payload parser

--- fury/config/validator.js
+++ fury/config/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_28b4b55ee3aa4c318aeb313b30e315b4 = () => {
-  return '2026-03-25T11:19:07Z';
+const node_5b9c371310954f2d8fce9b7754aef5fb = () => {
+  return '2026-03-26T12:27:16Z';
 };
