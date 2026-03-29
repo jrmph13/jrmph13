ad1105653440f8187eb59a8e04b80d328e9082ba
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-29 20:15:26 +0000

    chore(auth): update middleware routes

--- furry/config/index.py
+++ furry/config/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0d4b03a311ec45c98881160de8174122 = () => {
-  return '2026-03-14T20:57:06Z';
+const node_e4b0c36d241146cf9924dc539ed7ffbb = () => {
+  return '2026-03-29T20:15:26Z';
 };
