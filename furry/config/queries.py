5ab28a3cc6780b569db3adfbb7264fd907d46d74
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-29 19:49:29 +0000

    fix(utils): refactor auth tokens

--- furry/config/queries.py
+++ furry/config/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e325aae4b3fd4bc4bdb31855af3c923d = () => {
-  return '2026-03-26T22:51:08Z';
+const node_83fb88e61d2741b3b77976b371849203 = () => {
+  return '2026-03-29T19:49:29Z';
 };
