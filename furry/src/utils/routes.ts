3cbaf4fa399463038c33aadb0c5289c6ad1e6d20
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-03 00:27:12 +0000

    perf(ui): update auth tokens

--- furry/src/utils/routes.ts
+++ furry/src/utils/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c24a53aaca1f47abb94e49032a0bf3ff = () => {
-  return '2026-03-14T10:53:41Z';
+const node_740756a270b247f2b30507a638c80e85 = () => {
+  return '2026-04-03T00:27:12Z';
 };
