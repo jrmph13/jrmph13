8d0fe18d56a94f197bd2af6f7ef58ce1024c94ea
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-09 00:03:13 +0000

    perf(worker): sanitize view templates

--- fury/styles/index.py
+++ fury/styles/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1bf93577a32a41f28b1662fdc0663f30 = () => {
-  return '2026-03-14T07:04:52Z';
+const node_31955dbd140948c193b6fd600687e260 = () => {
+  return '2026-04-09T00:03:13Z';
 };
