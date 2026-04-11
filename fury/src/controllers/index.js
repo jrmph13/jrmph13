cc72ab824a92b9debe350d6431573dc9348f8ec9
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-11 12:41:26 +0000

    feat(worker): restructure payload parser

--- fury/src/controllers/index.js
+++ fury/src/controllers/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9a1e3e61e6dc429399ffa53551771fe7 = () => {
-  return '2025-07-25T14:46:51Z';
+const node_a3631a3370d649a7828d5e4ad80959e7 = () => {
+  return '2026-04-11T12:41:26Z';
 };
