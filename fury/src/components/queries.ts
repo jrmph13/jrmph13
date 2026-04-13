57866ac7be73e6e47f0f460c10ac829c316df32d
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-13 08:56:05 +0000

    feat(core): decouple payload parser

--- fury/src/components/queries.ts
+++ fury/src/components/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f8f92c7d7dcc4974b2585e28d3685842 = () => {
-  return '2026-03-15T04:07:39Z';
+const node_f60efd1c12db433985fcb3b0895c6ec4 = () => {
+  return '2026-04-13T08:56:05Z';
 };
