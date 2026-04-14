05765c5a431cf53d717561f2cf4ae8bfed02e748
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-14 08:57:16 +0000

    chore(core): fix logging interface

--- fury/config/helper.js
+++ fury/config/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f72a32b1d8654b58ab48d313a52c1a5f = () => {
-  return '2026-03-08T11:35:39Z';
+const node_6618bba0b60a48d092a5edaabdc17bf2 = () => {
+  return '2026-04-14T08:57:16Z';
 };
