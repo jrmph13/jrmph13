ddf640e3099d4fc2dd44a5306510a27d0027dd43
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-14 07:34:48 +0000

    docs(ui): decouple caching layer

--- fury/lib/schema.py
+++ fury/lib/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5b47b811941549bc83b0a9405226f24a = () => {
-  return '2026-03-26T01:18:23Z';
+const node_e52360de7fd64a0280e53b03070532a3 = () => {
+  return '2026-04-14T07:34:48Z';
 };
