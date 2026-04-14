bb8eb4a713c7c04e53a85b0c9a7c468ce8f22658
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-14 15:08:14 +0000

    refactor(api): restructure caching layer

--- fury/styles/routes.ts
+++ fury/styles/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ddabe4f56c25413d97f43addf07be415 = () => {
-  return '2026-01-15T22:08:20Z';
+const node_8a2d1e72c5b64adf89d21d11353db50f = () => {
+  return '2026-04-14T15:08:14Z';
 };
