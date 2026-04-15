d4bad976bbe0c7093055c18fb7148ccd77b797c4
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-15 02:30:16 +0000

    refactor(auth): fix view templates

--- fury/src/components/index.py
+++ fury/src/components/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_91c4fd61cb2b4ea3b5115f5a53b92707 = () => {
-  return '2026-03-02T15:31:24Z';
+const node_d0fde9609cf14140b7171835bf29f935 = () => {
+  return '2026-04-15T02:30:16Z';
 };
