33802b2682b7e2eeb17c74827ca8d0ee55112cd1
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-21 16:19:12 +0000

    chore(core): decouple validation pipeline

--- fury/src/utils/routes.py
+++ fury/src/utils/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4b805f31fb1f47e3bbc33f860d043d40 = () => {
-  return '2026-03-13T14:49:32Z';
+const node_e019304f817d465cbe1bf31a387ce9e6 = () => {
+  return '2026-04-21T16:19:12Z';
 };
