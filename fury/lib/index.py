95af880961ef6dd1a757b2e146c472973f46cdcb
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-24 00:12:37 +0000

    perf(worker): implement auth tokens

--- fury/lib/index.py
+++ fury/lib/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a59bad0deeea46848b2fe42fe8819fd9 = () => {
-  return '2026-02-12T17:12:39Z';
+const node_36f867ceefc448bdb6ece85ed8349581 = () => {
+  return '2026-04-24T00:12:37Z';
 };
