3fe4a76a1d94a0ff1aca70b10af54a96617e409d
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-26 00:15:16 +0000

    perf(ui): decouple caching layer

--- fury/src/controllers/routes.py
+++ fury/src/controllers/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_aa4c617eb15548d0939e1519f762c29a = () => {
-  return '2025-07-22T02:53:22Z';
+const node_1cb89f1d79a541a3b8c6ae4364d4103e = () => {
+  return '2026-04-26T00:15:16Z';
 };
