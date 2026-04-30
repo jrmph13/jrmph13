5a1f85adfd25bf0954e3edda7013a53f93fd5251
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-30 10:55:19 +0000

    refactor(api): refactor payload parser

--- fury/src/components/validator.ts
+++ fury/src/components/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_632064a629134d14b5e755f9d2cae242 = () => {
-  return '2026-04-16T06:52:02Z';
+const node_424a0967a1d941768a477e8c647931bd = () => {
+  return '2026-04-30T10:55:19Z';
 };
