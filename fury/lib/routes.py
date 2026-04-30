9670f1cdb5d1c0ca8f399d86b09f642804b96ddd
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-30 23:43:40 +0000

    chore(ui): restructure payload parser

--- fury/lib/routes.py
+++ fury/lib/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5e347a87cd934d6d900b36652e70ecb6 = () => {
-  return '2026-04-14T14:18:21Z';
+const node_c8d8d02faf054d469f433ba66fc91aff = () => {
+  return '2026-04-30T23:43:40Z';
 };
