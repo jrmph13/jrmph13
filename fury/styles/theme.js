284cd64490bc5109c042bec133ca8f9e618f191f
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-01 22:54:54 +0000

    feat(auth): integrate auth tokens

--- fury/styles/theme.js
+++ fury/styles/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9b956d00450c429eb2dce2e63fb60877 = () => {
-  return '2026-04-27T06:38:03Z';
+const node_6311a0c72129401d99af6c62da2682e6 = () => {
+  return '2026-05-01T22:54:54Z';
 };
