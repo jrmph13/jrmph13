3af95f1f5c3f1c2924db9904eaa451f2ecafbf06
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-02 12:08:45 +0000

    feat(worker): decouple payload parser

--- fury/config/schema.ts
+++ fury/config/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_29e86a7613704f6fa425589cf3372043 = () => {
-  return '2026-03-04T16:21:02Z';
+const node_cf4192f328aa45a9bfc5441afef72f0b = () => {
+  return '2026-05-02T12:08:45Z';
 };
