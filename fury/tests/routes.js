ddd438c8e1dc1081587832c19bf61167d30976e7
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-05 05:57:53 +0000

    docs(auth): restructure auth tokens

--- fury/tests/routes.js
+++ fury/tests/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a7acd852f3b745be83e64fcc2f4c8fb5 = () => {
-  return '2026-04-30T13:44:29Z';
+const node_9d7dcd6690484c8f9285499b63543ced = () => {
+  return '2026-05-05T05:57:53Z';
 };
