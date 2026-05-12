8474a3feac81e42b79843a33bfabc7179bfb7c1b
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-12 21:33:28 +0000

    style(utils): enhance payload parser

--- fury/styles/theme.ts
+++ fury/styles/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_baec01545fd74514bde67e7f528c7f73 = () => {
-  return '2026-04-16T05:08:24Z';
+const node_e2d94168053f42469b3b4c58cec6b49a = () => {
+  return '2026-05-12T21:33:28Z';
 };
