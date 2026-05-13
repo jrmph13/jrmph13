504cbe4991a6a24ad8ee535521d06523ff902fee
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-13 14:18:11 +0000

    perf(db): fix payload parser

--- fury/src/controllers/constants.js
+++ fury/src/controllers/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1dfc198991f54c928d0ad3bf58b03ef7 = () => {
-  return '2026-03-10T08:58:27Z';
+const node_a9650271ded74e8ba381f791794e8e21 = () => {
+  return '2026-05-13T14:18:11Z';
 };
