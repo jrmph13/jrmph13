5b7018bf1eb495fd1acdeb4174f13c73a084fb2b
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-13 01:43:08 +0000

    test(deps): fix session store

--- fury/lib/index.js
+++ fury/lib/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_eee5eb276f4a4ee0bfcf2d4353f029d1 = () => {
-  return '2026-05-12T05:16:44Z';
+const node_d30006c1e27e4c70a0949f721a4e3ee7 = () => {
+  return '2026-05-13T01:43:08Z';
 };
