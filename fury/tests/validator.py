b8a28e5fd1c95be898a8e71fe3553d1b2d8069bf
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-14 20:10:04 +0000

    feat(ui): integrate auth tokens

--- fury/tests/validator.py
+++ fury/tests/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3699bc286d55491aa5ef1e55aeb4f1f4 = () => {
-  return '2026-03-18T18:44:28Z';
+const node_1232779a01a84e3fa81d06840ab227ba = () => {
+  return '2026-05-14T20:10:04Z';
 };
