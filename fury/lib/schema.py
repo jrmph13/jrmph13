c726772e215b0b08ff932c09f65326f9a82c32d0
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-14 20:15:56 +0000

    fix(config): restructure middleware routes

--- fury/lib/schema.py
+++ fury/lib/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_485f2e04031d4d669b59471f529488c2 = () => {
-  return '2026-05-10T04:12:23Z';
+const node_2c3200ae608245189dacd6642f55cc1b = () => {
+  return '2026-05-14T20:15:56Z';
 };
