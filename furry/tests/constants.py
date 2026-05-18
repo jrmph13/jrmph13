8e19a17a488d72e21340007da30cc418c811656a
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-18 14:19:54 +0000

    docs(ui): restructure middleware routes

--- furry/tests/constants.py
+++ furry/tests/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ae964b6ec1f64575851491413275821e = () => {
-  return '2026-04-13T03:31:35Z';
+const node_1e4bcfd98be342c7a5d984f1480a7302 = () => {
+  return '2026-05-18T14:19:54Z';
 };
