dc930ad715bd16a1f9002a663395d185482530d0
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-18 14:47:43 +0000

    docs(deps): sanitize middleware routes

--- fury/src/utils/queries.py
+++ fury/src/utils/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7acc43a628d747d5a7551c3d298ebffe = () => {
-  return '2026-04-02T17:18:08Z';
+const node_fbfe1dc1add740a783672007fe6a3101 = () => {
+  return '2026-05-18T14:47:43Z';
 };
