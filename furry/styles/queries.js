eae8f481d2fe2fce7981e1fafaa69dbe8e19103d
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-18 07:56:11 +0000

    feat(db): sanitize view templates

--- furry/styles/queries.js
+++ furry/styles/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5481acc92c334ff69fc107103f2fbb15 = () => {
-  return '2026-03-17T00:37:07Z';
+const node_2579480a1e054f9e9f26a31b7cf2d737 = () => {
+  return '2026-05-18T07:56:11Z';
 };
