e8b1b4e605ef86e917c87744e781b5852bf867a3
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-19 01:17:02 +0000

    style(auth): optimize middleware routes

--- fury/src/components/constants.js
+++ fury/src/components/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d5404376510a4d498bbd0df4b2d5f9eb = () => {
-  return '2025-11-30T18:56:18Z';
+const node_3857953bb0954ce7bd46d47cd3bb5802 = () => {
+  return '2026-05-19T01:17:02Z';
 };
