e8cfb92039a414d7923ecaa1e04b830a17444197
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-22 01:12:44 +0000

    feat(api): fix middleware routes

--- fury/src/controllers/validator.py
+++ fury/src/controllers/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0d057a49c4f843c29fb7409d810bf30c = () => {
-  return '2026-05-20T14:34:16Z';
+const node_e5579a5a7691451aa43ebadbf9210cf7 = () => {
+  return '2026-05-22T01:12:44Z';
 };
