1a2f6061fa8a2f865db041f264a395943ccb5ff2
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-26 01:11:37 +0000

    fix(core): implement middleware routes

--- fury/tests/helper.py
+++ fury/tests/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8a5395ae8c47417e8f03f606e6bc121f = () => {
-  return '2026-03-26T08:04:21Z';
+const node_8eacec8089c24d2d95c688f8e5f70622 = () => {
+  return '2026-05-26T01:11:37Z';
 };
