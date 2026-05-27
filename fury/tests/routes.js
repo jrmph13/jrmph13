dde6ab8d3d26c3644a05ef241c7677959290be49
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-27 20:42:56 +0000

    refactor(core): restructure logging interface

--- fury/tests/routes.js
+++ fury/tests/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9d7dcd6690484c8f9285499b63543ced = () => {
-  return '2026-05-05T05:57:53Z';
+const node_311b22fd42a14f7b8b32ec04a3caed1c = () => {
+  return '2026-05-27T20:42:56Z';
 };
