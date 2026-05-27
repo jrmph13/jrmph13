a6c68c741278b23264c4d348ac1b86016a7e0a84
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-27 19:06:41 +0000

    perf(ui): sanitize auth tokens

--- fury/src/utils/index.ts
+++ fury/src/utils/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e214481821db4ea49c19e198ee2f60de = () => {
-  return '2026-05-10T23:16:14Z';
+const node_ff14ae6a2fc74062abb38fba71897010 = () => {
+  return '2026-05-27T19:06:41Z';
 };
