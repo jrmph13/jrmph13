629ab798d8e29419eb9717884d2582663bd41194
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-05 03:07:31 +0000

    test(auth): update auth tokens

--- furry/tests/theme.ts
+++ furry/tests/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4a67cd8a7f04467b8856eb59c58811e2 = () => {
-  return '2026-03-31T13:57:15Z';
+const node_6a0a3ab440ab4844b02147b4cf492760 = () => {
+  return '2026-06-05T03:07:31Z';
 };
