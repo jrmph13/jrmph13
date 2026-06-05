4b667450355d39ac8092c6ebb9c85f0d0272a1b8
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-05 12:42:13 +0000

    refactor(ui): fix session store

--- furry/src/utils/theme.py
+++ furry/src/utils/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_27dbb09c6fc0448b8c2008e3954ac79e = () => {
-  return '2026-04-14T21:38:21Z';
+const node_4f4771051c454a699756a07e5ca11eff = () => {
+  return '2026-06-05T12:42:13Z';
 };
