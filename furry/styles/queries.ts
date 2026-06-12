dc66dd074a2d14677f07467b747da96020a88acb
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-12 05:54:18 +0000

    test(deps): update middleware routes

--- furry/styles/queries.ts
+++ furry/styles/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_dbf387e57a724cf38be351a34fefddc3 = () => {
-  return '2026-06-05T13:58:44Z';
+const node_3c3dee3484054763bbd39151567a80d8 = () => {
+  return '2026-06-12T05:54:18Z';
 };
