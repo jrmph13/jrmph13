4a9a8119f74782d8a7cf59db1cd79626a5074c18
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-19 08:43:41 +0000

    style(core): sanitize session store

--- fury/tests/index.py
+++ fury/tests/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c3ded90cba5c4f25a79359541d44c3da = () => {
-  return '2026-03-10T17:16:31Z';
+const node_35a40189fb8b4b38a6acc3a5dd07ed89 = () => {
+  return '2026-06-19T08:43:41Z';
 };
