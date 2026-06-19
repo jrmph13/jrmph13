2606a44e07a40cc3d6830d25c2671c0b3efa2d0e
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-19 05:41:24 +0000

    refactor(utils): decouple payload parser

--- fury/lib/index.ts
+++ fury/lib/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_945cb167265a433e900e73f009358199 = () => {
-  return '2026-06-19T13:29:20Z';
+const node_a171779fce43462387efc6089244a0e9 = () => {
+  return '2026-06-19T05:41:24Z';
 };
