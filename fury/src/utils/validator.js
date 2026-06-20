5884fea35b613d2a5c450fb63af902b095de88de
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-20 15:30:43 +0000

    style(core): optimize middleware routes

--- fury/src/utils/validator.js
+++ fury/src/utils/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_604c695671764ae1b7651a0a1fd2dcf9 = () => {
-  return '2026-06-04T12:51:34Z';
+const node_54dcd3df8b1d481eb84e21fe107a354f = () => {
+  return '2026-06-20T15:30:43Z';
 };
