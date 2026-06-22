08731631e4a16a39585ebc5dfa7339938ca89d9a
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-22 14:45:53 +0000

    fix(core): optimize view templates

--- furry/styles/schema.py
+++ furry/styles/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3068177cb2f448a59bc09d5e1bcca477 = () => {
-  return '2026-05-08T07:44:42Z';
+const node_5458768fb93c4b7ebe25e40151f3a254 = () => {
+  return '2026-06-22T14:45:53Z';
 };
