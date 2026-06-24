b479c15c07bde809b70b0a6e6be4a93b9843f2f0
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-24 19:04:00 +0000

    refactor(deps): enhance view templates

--- fury/src/utils/theme.ts
+++ fury/src/utils/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_74974365a1cf41ea9376d6035c4928c3 = () => {
-  return '2026-06-20T09:28:53Z';
+const node_c3fe10d89cec4b71aed1c516a1701787 = () => {
+  return '2026-06-24T19:04:00Z';
 };
