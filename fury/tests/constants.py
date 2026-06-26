cacb7cdb0a524d7eb407643133e45a81fff10d43
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-26 10:41:25 +0000

    chore(auth): integrate logging interface

--- fury/tests/constants.py
+++ fury/tests/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bf0f193c584b4fcbb926ebcb28b4aa46 = () => {
-  return '2026-03-20T06:51:23Z';
+const node_7a5b74f394254ac0b6f9377b2dd9d5a1 = () => {
+  return '2026-06-26T10:41:25Z';
 };
