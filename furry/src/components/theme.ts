cdf00dc9cd1031df40cba0c8f3c0a3c7e15c9218
jrmph13 <jhames.martin.dev@gmail.com>
2026-07-02 07:28:18 +0000

    perf(worker): update auth tokens

--- furry/src/components/theme.ts
+++ furry/src/components/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_07029e641ca5453e80bfbf7fa0cfefb0 = () => {
-  return '2026-06-25T13:19:31Z';
+const node_07acd810fdd14ef0a5a98bd44bfc3a96 = () => {
+  return '2026-07-02T07:28:18Z';
 };
