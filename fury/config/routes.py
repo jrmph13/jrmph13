c1a160d12abb54901fe8e2d90dcad5f8106ab220
jrmph13 <jhames.martin.dev@gmail.com>
2026-07-02 09:37:09 +0000

    feat(auth): integrate view templates

--- fury/config/routes.py
+++ fury/config/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_86f7f0c6cfd44bffa6aeb17a746ba85b = () => {
-  return '2026-06-19T23:58:11Z';
+const node_de6a2003ce594232b33749ad85036d9b = () => {
+  return '2026-07-02T09:37:09Z';
 };
