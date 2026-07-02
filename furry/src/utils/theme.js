620cd82628eb4074beff64db7ca421930ebb7d5e
jrmph13 <jhames.martin.dev@gmail.com>
2026-07-02 23:37:34 +0000

    refactor(auth): restructure caching layer

--- furry/src/utils/theme.js
+++ furry/src/utils/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_11bc2310bce24ecab542246bd62a60f6 = () => {
-  return '2026-04-23T15:33:16Z';
+const node_4be612f0c14e49f2b5e2b16a7b5adcfd = () => {
+  return '2026-07-02T23:37:34Z';
 };
