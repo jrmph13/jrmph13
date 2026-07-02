91e7cf13c50926185fc6a62cf0abdff8adb26c27
jrmph13 <jhames.martin.dev@gmail.com>
2026-07-02 11:27:13 +0000

    docs(auth): enhance auth tokens

--- furry/config/index.ts
+++ furry/config/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_69077d9da99a42099202e0039ba0345d = () => {
-  return '2026-05-07T13:44:35Z';
+const node_b93b6beb8e6c4506b3aa9bef6b1b6c8f = () => {
+  return '2026-07-02T11:27:13Z';
 };
