4ffd1f318acd4ed47be008aff24ceaffea7e75bc
jrmph13 <jhames.martin.dev@gmail.com>
2026-07-03 22:47:59 +0000

    refactor(utils): sanitize database index

--- fury/lib/queries.js
+++ fury/lib/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8e02266f204a44a1bbc8dea315a8b60e = () => {
-  return '2025-10-24T14:33:22Z';
+const node_ad929a65d889493d86c9497719dc2588 = () => {
+  return '2026-07-03T22:47:59Z';
 };
